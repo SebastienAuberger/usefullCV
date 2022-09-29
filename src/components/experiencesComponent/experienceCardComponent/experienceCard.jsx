af303de2c302691c8f7da46ad849c47ffc08b49b
SebastienAuberger <sebastien?abg@gmail.com>
2022-09-29 16:27:45 +0200

    DEV: feat - Add  experience cards structure

---
+++ src/components/experiencesComponent/experienceCardComponent/experienceCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class ExperienceCard extends React.Component {
+    render(){
+        return(
+            <div className="experienceCard">
+                <h2 className="cardTitle">DEVELOPPEUR JUNIOR FULLSTACK</h2>
+                <h3 className="companyName">MERIM SERVICE</h3>
+                <h4 className="cityName">CHATEAUROUX</h4>
+            </div>
+        )
+    }
+}
--- src/components/experiencesComponent/experiences.jsx
+++ src/components/experiencesComponent/experiences.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import ExperiencesCard from "./experienceCardComponent/experienceCard"
+import "./experience.css"
 export default class Experiences extends React.Component{
 
     render(){
         return(
             <div className="main experiences smallShadow">
                 <h1>Experiences</h1>
+                <div className="experienceCards">
+                    <ExperiencesCard />
+                </div>
             </div>
         )
     }
