0a79b03e290a715e2cacdae6d4666eeb756f9c0b
SebastienAuberger <sebastien.abg@gmail.com>
2022-09-30 21:00:54 +0200

    DEV-5: ETUDES_COMPONENT: ETUDES_CARD_COMPONENT: feat - Add etudesCard

--- src/components/etudesComponent/etudes.jsx
+++ src/components/etudesComponent/etudes.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-
+import cv from "../../shared/cvInfo.json"
+import EtudeCard from "./etudesCardComponent/etudeCard";
+import "./etudes.css"
 export default class Etudes extends React.Component{
 
     render(){
         return(
             <div className="main etudes smallShadow">
-                <h1>Etudes</h1>
+                <h1>Experiences</h1>
+                <div className="etudeCards">
+                    {
+                        cv.category[1].content.cards.map((el) => {
+                               return(<EtudeCard degree={el.degree} school={el.school} city={el.city} date={el.date} />)
+                        })
+                    }
+                </div>
             </div>
         )
     }
